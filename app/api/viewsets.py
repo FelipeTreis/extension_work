96fd4b7b86a87d5517a80c98431b352540202d24
Felipe Treis <felipetreis1@gmail.com>
2022-09-20 15:28:22 -0300

    Created views sets

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,4 +1,7 @@
-from app.api.serializers import UserSerializer
+from app.api.serializers import (AutoModelSerializer, BrandSerializer,
+                                 MaintenanceSerializer, ServiceSerializer,
+                                 UserSerializer, VehicleSerializer)
+from app.models import AutoModel, Brand, Maintenance, Service, Vehicle
 from decouple import config
 from django.contrib.auth.models import User
 from rest_framework import viewsets
@@ -12,4 +15,34 @@ class Pagination(PageNumberPagination):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
+    pagination_class = Pagination
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.filter(is_active=True)
+    serializer_class = BrandSerializer
+    pagination_class = Pagination
+
+
+class AutoModelViewSet(viewsets.ModelViewSet):
+    queryset = AutoModel.objects.filter(is_active=True)
+    serializer_class = AutoModelSerializer
+    pagination_class = Pagination
+
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.filter(is_active=True)
+    serializer_class = VehicleSerializer
+    pagination_class = Pagination
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.filter(is_active=True)
+    serializer_class = ServiceSerializer
+    pagination_class = Pagination
+
+
+class MaintenanceViewSet(viewsets.ModelViewSet):
+    queryset = Maintenance.objects.filter(is_active=True)
+    serializer_class = MaintenanceSerializer
     pagination_class = Pagination
