52b8a99a59be89e0926037708064917b7e12b980
Felipe Treis <felipetreis1@gmail.com>
2022-09-23 16:15:14 -0300

    Added permissions

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -2,10 +2,12 @@ from app.api.serializers import (AutoModelSerializer, BrandSerializer,
                                  MaintenanceSerializer, ServiceSerializer,
                                  UserSerializer, VehicleSerializer)
 from app.models import AutoModel, Brand, Maintenance, Service, Vehicle
+from app.permissions import IsOwner
 from decouple import config
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class Pagination(PageNumberPagination):
@@ -16,33 +18,39 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
     pagination_class = Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.filter(is_active=True)
     serializer_class = BrandSerializer
     pagination_class = Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
 class AutoModelViewSet(viewsets.ModelViewSet):
     queryset = AutoModel.objects.filter(is_active=True)
     serializer_class = AutoModelSerializer
     pagination_class = Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.filter(is_active=True)
     serializer_class = VehicleSerializer
     pagination_class = Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.filter(is_active=True)
     serializer_class = ServiceSerializer
     pagination_class = Pagination
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
 class MaintenanceViewSet(viewsets.ModelViewSet):
     queryset = Maintenance.objects.filter(is_finished=True)
     serializer_class = MaintenanceSerializer
     pagination_class = Pagination
+    permission_classes = [IsOwner, ]
