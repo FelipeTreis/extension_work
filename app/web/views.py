0c41f6c957ed1f280342dbadc73f10a48db7a5b9
Felipe Treis <felipetreis1@gmail.com>
2022-10-11 15:52:48 -0300

    Added context in view

--- app/web/views.py
+++ app/web/views.py
@@ -1,7 +1,8 @@
+from app.models import Maintenance
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -11,17 +12,17 @@ def home(request):
 
 
 def send_email(request):
-    html_content = render_to_string(
-        'app/emails/next_maintenance.html',
-        context={
-            'name': 'Felipe',
-            'next_date': '2022-12-25',
-        }
-    )
+    data = Maintenance()
+
+    mail_title = 'Maintenance'
+    html_content = render_to_string('app/emails/next_maintenance.html', context={
+        'request': request,
+        'data': data,
+    })
     text_content = strip_tags(html_content)
 
     email = EmailMultiAlternatives(
-        'Maintenance', text_content, settings.EMAIL_HOST_USER,
+        mail_title, text_content, settings.EMAIL_HOST_USER,
         ['felipetreis1@gmail.com']
     )
     email.attach_alternative(html_content, 'text/html')
