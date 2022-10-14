52e49f89c35c11d0243adfd6ce39c66973c47a24
Felipe Treis <felipetreis1@gmail.com>
2022-10-14 13:09:11 -0300

    Dynamics data created for sending email

--- app/web/urls.py
+++ app/web/urls.py
@@ -5,5 +5,5 @@ app_name = 'web'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('send-email/', views.send_email, name='send_email')
+    path('send-email/<int:id>/', views.send_email, name='send_email')
 ]
--- app/web/views.py
+++ app/web/views.py
@@ -11,8 +11,8 @@ def home(request):
     return render(request, 'app/pages/home.html')
 
 
-def send_email(request):
-    data = Maintenance()
+def send_email(request, id):
+    data = get_object_or_404(Maintenance, pk=id, is_finished=True)
 
     mail_title = 'Maintenance'
     html_content = render_to_string('app/emails/next_maintenance.html', context={
@@ -21,7 +21,7 @@ def send_email(request):
     })
     text_content = strip_tags(html_content)
 
-    email = EmailMultiAlternatives(mail_title, text_content, settings.EMAIL_HOST_USER, ['felipetreis1@gmail.com'])
+    email = EmailMultiAlternatives(mail_title, text_content, settings.EMAIL_HOST_USER, [request.user.email])
     email.attach_alternative(html_content, 'text/html')
     email.send()
 
