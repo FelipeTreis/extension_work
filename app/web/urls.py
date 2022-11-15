b70a8439a802678bdfd9b844db0592e1d91a6bb9
Felipe Treis <felipetreis1@gmail.com>
2022-11-14 23:21:23 -0300

    Created url to maintenance

--- app/web/urls.py
+++ app/web/urls.py
@@ -1,9 +1,11 @@
-from app.web import views
 from django.urls import path
+
+from app.web import views
 
 app_name = 'web'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('maintenance/<int:id>/', views.content, name='content'),
     path('send-email/<int:id>/', views.send_email, name='send_email')
 ]
